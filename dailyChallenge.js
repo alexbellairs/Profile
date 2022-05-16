137d9c5a784752b8e8dc7b9a7fc12c26b4a1023a
alexbellairs <alexanderbellairs@gmail.com>
2022-05-16 14:26:48 +0100

    first commit

---
+++ dailyChallenge.js
@@ -0,0 +1,8 @@
+// let reverse = (str) => {
+//   let newString = "";
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     newString += str[i];
+//   }
+//   console.log(newString);
+// };
+// reverse("Hello My Name Is Alex");
